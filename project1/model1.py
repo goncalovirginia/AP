0459fcf0d9f4ef2c06af63292f929bc2489d7c86
xXParagoNXx <gonvirginia.blueslime@gmail.com>
2024-04-07 23:43:30 +0100

    model1 small changes

--- project1/model1.py
+++ project1/model1.py
@@ -7,21 +7,21 @@ import pandas as pd
 import numpy as np
 from torch.utils.data import Dataset, DataLoader
 import matplotlib.pyplot as plt
-
 import CustomImageDataset as CID
+
 # Load the train dataset
-train_dataset = CID.CustomImageDataset(annotations_file='./images/train.csv', img_dir='./images/train/',transform=preprocess)
+train_dataset = CID.CustomImageDataset(annotations_file='./images/train.csv', img_dir='./images/train/', transform=preprocess)
 train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
 
 # Load the test dataset
-test_dataset = CID.CustomImageDataset(annotations_file='./images/test.csv', img_dir='./images/test/',transform=preprocess)
+test_dataset = CID.CustomImageDataset(annotations_file='./images/test.csv', img_dir='./images/test/', transform=preprocess)
 test_loader = DataLoader(test_dataset, batch_size=64, shuffle=True)
 
 # Model
 
 
-y_pred = "prediction";
+y_predict = "y_predict";
 
-# Save in dataframe for Kaggle submission
-submission = pd.DataFrame({'Id': test_dataset.img_labels.iloc[:, 0], 'main_type': y_pred})
+# Save y_predict in dataframe for Kaggle submission
+submission = pd.DataFrame({'Id': test_dataset.img_labels.iloc[:, 0], 'main_type': y_predict})
 submission.to_csv('./submission.csv', index=False)
