09f912ef12139c61982ec872c744c1cd08baff47
xXParagoNXx <gonvirginia.blueslime@gmail.com>
2024-04-08 01:52:13 +0100

    project1 script skeleton setup

--- project1/model1.py
+++ project1/model1.py
@@ -1,4 +1,5 @@
 import torch
+import torchvision
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 from torchvision.io import read_image
@@ -8,20 +9,103 @@ import numpy as np
 from torch.utils.data import Dataset, DataLoader
 import matplotlib.pyplot as plt
 import CustomImageDataset as CID
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from datetime import datetime
+
+# Constants
+
+IMAGES_PATH = "project1/images/"
+BATCH_SIZE = 8
 
 # Load the train dataset
-train_dataset = CID.CustomImageDataset(annotations_file='./images/train.csv', img_dir='./images/train/', transform=preprocess)
-train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+
+train_dataset = CID.CustomImageDataset(annotations_file=IMAGES_PATH+'train.csv', img_dir=IMAGES_PATH+'train/')#, transform=preprocess)
+train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
 
 # Load the test dataset
-test_dataset = CID.CustomImageDataset(annotations_file='./images/test.csv', img_dir='./images/test/', transform=preprocess)
-test_loader = DataLoader(test_dataset, batch_size=64, shuffle=True)
 
-# Model
+test_dataset = CID.CustomImageDataset(annotations_file=IMAGES_PATH+'test.csv', img_dir=IMAGES_PATH+'test/')#, transform=preprocess)
+test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=True)
 
+# Show some images
+
+def imshow(img):
+    img = img / 2 + 0.5 # Unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+dataiter = iter(train_loader)
+images, labels = next(dataiter)
+imshow(torchvision.utils.make_grid(images))
+print(' '.join(f'{labels[j]}    ' for j in range(BATCH_SIZE)))
+
+# CNN Model
+
+class CNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+cnn = CNN()
+
+# Loss function and optimizer
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(cnn.parameters(), lr=0.001, momentum=0.9)
+
+# Training
+
+for epoch in range(2):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+            running_loss = 0.0
+
+print('Finished Training')
+
+# Save model
+
+current_datetime = datetime.now()
+torch.save(cnn.state_dict(), 'project1/models/CNN{current_datetime}.pth')
+
+# Predictions
 
 y_predict = "y_predict";
 
-# Save y_predict in dataframe for Kaggle submission
+# Save y_predict in .csv
+
 submission = pd.DataFrame({'Id': test_dataset.img_labels.iloc[:, 0], 'main_type': y_predict})
-submission.to_csv('./submission.csv', index=False)
+submission.to_csv('submission.csv', index=False)
